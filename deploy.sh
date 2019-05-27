d3ace542d2eb3e36e9aac8112e1d1b4465782104
gjvera <git@gabrieljosephvera.com>
2019-05-26 22:40:50 -0700

    rebuilding site Sun 26 May 2019 10:40:50 PM MST

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo -e "\033[0;32mDeploying Updates to Github...\033[0m"
 hugo
 msg="rebuilding site `date`"
 git add .
-git commit -m "msg" .
+git commit -m "$msg" .
 git push -f origin master 
 cd public
 git add .
